cea6c6b17c11c611ed71f65c36515013eb5d1bb5
Aaron Byrom <aaronbyrom@protonmail.com>
2021-11-02 20:42:15 +1100

    forgot data

---
+++ index.js
@@ -0,0 +1,25 @@
+const serverData = {
+  weather: [
+    {
+      temp: 22,
+      cloudHeight: 300,
+    },
+    {
+      temp: 27,
+      cloudHeight: 300,
+    },
+    {
+      temp: 21,
+      cloudHeight: 300,
+    },
+    {
+      temp: 19,
+      cloudHeight: 300,
+    },
+    {
+      temp: 18,
+      cloudHeight: 300,
+    }
+  ],
+  id: 'whatever12345',
+}
