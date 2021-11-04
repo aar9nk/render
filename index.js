6fd61a3e87fa2302076ac3746136fd87d53cf321
Aaron Byrom <aaronbyrom@protonmail.com>
2021-11-04 20:41:41 +1100

    quote finished

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const getWeatherData = async (city) => {
-   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1d746361df4112d4a44339cc2fabd41b`);
-   const data = await response.json();
-   return data;
+const getQuote = async () => {
+  const response = await fetch('https://api.quotable.io/random');
+  const data = await response.json();
+  return data;
 }
 
 const form = document.querySelector('#form');
-const input = document.querySelector('#city-input');
 const list = document.querySelector('#weather-list');
 
-const createWeatherData = (item) => { 
-  render(item.main.temp - 270);
-}
-
-const createHtml = (text) => {
-  return `
-    <li class="primary">${text}</li>
+const render = (quote) => {
+  list.innerHTML = `
+    <li>${quote.content}, by ${quote.author}</li>
   `;
-}
-
-const render = (item) => {
-  const result = createHtml(item);
-
-  list.innerHTML = result;
 }
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const temp = await getWeatherData(input.value);
-  createWeatherData(temp);
-});
+  const quote = await getQuote();
+  render(quote);
+}) 
